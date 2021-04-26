e18fce484f917ae2065b7371557568e6388c55dc
Micha Alt <mail@lean-stack.de>
2021-04-26 08:13:56 +0200

    Implement sample starter.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
 // Import styles to let vite process them
 import '../css/styles.css';
+import { countPrimeNumbers } from './lib/primes';
 
 // Starting point. Enjoy the ride.
+
+const maxValueFld = document.getElementById('maxValue');
+const testsContainer = document.querySelector('main');
+
+let maxValue = 10000;
+maxValueFld.value = maxValue;
+maxValueFld.addEventListener('input', () => {
+  maxValue = parseInt(maxValueFld.value);
+});
+
+testsContainer.addEventListener('click', (ev) => {
+  if( ev.target.matches('button')) {
+    const startBtn = ev.target;
+    startBtn.disabled = true;
+    setTimeout(() => {
+      const elapsedMilliseconds = timeThis(() => {
+        countPrimeNumbers(1, maxValue);
+      });
+      startBtn.previousElementSibling.querySelector('span').textContent = elapsedMilliseconds.toFixed(0);
+      startBtn.disabled = false;
+    }, 50);
+  }
+});
+
+function timeThis(action) {
+  const start = performance.now();
+  action();
+  const end = performance.now();
+  return end - start;
+}
